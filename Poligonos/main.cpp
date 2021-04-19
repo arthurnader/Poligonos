b06c379488d254cf96146e88b034e039de5ec187
arthurnader <82470629+arthurnader@users.noreply.github.com>
2021-04-19 17:29:44 -0300

    We made the function responsible to determine if the polygon is convex or not.

--- Poligonos/main.cpp
+++ Poligonos/main.cpp
@@ -5,6 +5,27 @@ using namespace std;
 
 
 int main() {
-	
+	int numeroPontos;
+	vector<Point> pontos;
+
+
+	cout << "Digite o numero de vertices do poligo: ";
+	cin >> numeroPontos;
+	cout << "Agora, entre com as coordenadas de cada vertice em sentido anti-horario: ";
+
+	double x = 0; double y = 0;
+
+	for (int i = 0; i != numeroPontos; i++) {
+		Point pnt(NULL, NULL, 0);
+		cin >> x;
+		cin >> y;
+		pnt.set_x(x);
+		pnt.set_y(y);
+		pontos.push_back(pnt);
+	}
+
+	Poligono* pol = new Poligono(pontos);
+	cout << pol->ehConvexo() << endl;
+
 	return 0;
 }
